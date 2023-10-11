341d84526f7662dcf98dd2f6344ae027838589a8
Ryan P. Nicholl <rnicholl@protonmail.com>
2023-10-10 19:28:59 -0700

    Merge Entities test

--- rylang/rylang/tests/gtests.cpp
+++ rylang/rylang/tests/gtests.cpp
@@ -16,7 +16,18 @@ TEST(rylang_modules, merge_entities)
 
     rylang::entity_ast e(rylang::class_entity_ast{}, false, {{"foo", rylang::entity_ast{rylang::class_entity_ast{}, false, {}}}, {"bar", rylang::entity_ast{rylang::class_entity_ast{}, false, {}}}});
 
+    rylang::entity_ast e2(rylang::class_entity_ast{}, false, {{"foo", rylang::entity_ast{rylang::class_entity_ast{}, false, {}}}});
+
+    ASSERT_NE(a, b);
+    ASSERT_NE(a, c);
+    ASSERT_NE(b, c);
     rylang::merge_entity(c, a);
+
+    ASSERT_EQ(c, a);
+    ASSERT_EQ(c, e2);
+    ASSERT_NE(c, b);
+    ASSERT_NE(c, e);
+
     rylang::merge_entity(c, b);
 
     ASSERT_EQ(c, e);
