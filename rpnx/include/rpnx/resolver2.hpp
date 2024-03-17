dfbf1433259a579ad3c41ba685afe2cc34a68f6d
Ryan P. Nicholl <rnicholl@protonmail.com>
2024-03-16 18:30:53 -0700

    Add some draft work for a version 2 of the resolver system.
    I'd like it to be able to compile the resolvers in a manner that I don't have to recompile all objects every time I alter a single header.
    
    Also, to implement caching, I will be implementing a  serialization system.

--- rpnx/include/rpnx/resolver2.hpp
+++ rpnx/include/rpnx/resolver2.hpp
@@ -8,12 +8,17 @@ namespace rpnx
 
     // A pure question is a question that depends only on the input and
     // any sub-questions it may ask.
+    // It can be cached between runs if the executable version is the same.
+    // Note the dependencies don't have to be pure, only the question itself.
+    // Example: "class_placement_info_question"
     struct pure_question_tag
     {
     };
 
     // An impure question depends upon the state of the system,
     // other than the input and sub-questions.
+    // Cannot be cached between runs.
+    // Example: "input_file_list_question"
     struct impure_question_tag
     {
     };
