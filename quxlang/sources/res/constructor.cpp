70ddd16e8ac7389d65b1969ed361fba51e803839
Ryan Nicholl <rnicholl@protonmail.com>
2024-12-26 12:31:28 -0800

    GCC/MSVC compilation issues

--- quxlang/sources/res/constructor.cpp
+++ quxlang/sources/res/constructor.cpp
@@ -27,7 +27,7 @@ QUX_CO_RESOLVER_IMPL_FUNC_DEF(nontrivial_default_dtor)
         if (candidate)
         {
             auto dtor_selection = selection_reference{.templexoid = input, .overload = ol};
-            auto dtor_instanciation = instantiation_type{.parameters = *candidate, .callee = dtor_selection};
+            auto dtor_instanciation = instantiation_type{.callee = dtor_selection, .parameters = *candidate};
 
             co_return dtor_instanciation;
         }
@@ -50,17 +50,15 @@ QUX_CO_RESOLVER_IMPL_FUNC_DEF(nontrivial_default_dtor)
             if (member_has_nontrivial_dtor)
             {
                 // We need a built-in destructor
-                auto builtin_dtor_selection = selection_reference{.templexoid = input, .overload = {.call_parameters = dtor_call_type, .builtin = true}};
+                auto builtin_dtor_selection = selection_reference{.templexoid = input, .overload = {.builtin = true, .call_parameters = dtor_call_type}};
 
-                auto builtin_dtor_instanciation = instantiation_type{.parameters = dtor_call_type, .callee = builtin_dtor_selection};
+                auto builtin_dtor_instanciation = instantiation_type{.callee = builtin_dtor_selection, .parameters = dtor_call_type, };
 
                 co_return builtin_dtor_instanciation;
             }
-
-
         }
 
-         // TODO: Inheritance?
+        // TODO: Inheritance?
     }
 
     co_return std::nullopt;
