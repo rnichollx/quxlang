f053e0f0d5547571aaffdc2cb3737338387b2717
Ryan P. Nicholl <rnicholl@protonmail.com>
2026-06-07 18:41:14 -0700

    Try to get better performance when compiling.

--- quxlang/sources/queries/run_static_tests.cpp
+++ quxlang/sources/queries/run_static_tests.cpp
@@ -10,10 +10,29 @@ rpnx::querygraph::coroutine< quxlang::run_static_tests_spec > quxlang::run_stati
         co_return std::monostate{};
     }
 
+    // To get better performance, yield dependencies so they can be processed in separate threads
     for (std::pair< std::string const, module_configuration > const& module_entry : target_config.module_configurations)
     {
         type_symbol const module_symbol = absolute_module_reference{.module_name = module_entry.first};
-        std::set< type_symbol > const tests = co_await rpnx::querygraph::request< list_static_tests_query >(module_symbol);
+        co_yield rpnx::querygraph::dependency< list_static_tests_query >(module_symbol);
+    }
+
+    // Now, for each module, we should yield dependencies on each test
+    for (std::pair< std::string const, module_configuration > const& module_entry : target_config.module_configurations)
+    {
+        type_symbol const module_symbol = absolute_module_reference{.module_name = module_entry.first};
+        std::set< type_symbol > const & tests = co_await rpnx::querygraph::request< list_static_tests_query >(module_symbol);
+        for (type_symbol const& test : tests)
+        {
+            co_yield rpnx::querygraph::dependency< run_static_test_query >(test);
+        }
+    }
+
+    // finally, we actually gather the results of each test by repeating the loop with co_await instead of co_yield
+    for (std::pair< std::string const, module_configuration > const& module_entry : target_config.module_configurations)
+    {
+        type_symbol const module_symbol = absolute_module_reference{.module_name = module_entry.first};
+        std::set< type_symbol > const & tests = co_await rpnx::querygraph::request< list_static_tests_query >(module_symbol);
         for (type_symbol const& test : tests)
         {
             co_await rpnx::querygraph::request< run_static_test_query >(test);
